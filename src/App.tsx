10022f4f04f3455fd38517a2b1b70b61d8ab95c6
maheenur13 <jahidun.nur@apsissolutions.com>
2023-08-17 17:49:33 +0600

    "Initial commit from Create Refine App"

---
+++ src/App.tsx
@@ -0,0 +1,44 @@
+import { GitHubBanner, Refine } from "@refinedev/core";
+import {
+    notificationProvider,
+    WelcomePage,
+    RefineThemes,
+} from "@refinedev/antd";
+import dataProvider from "@refinedev/simple-rest";
+import routerProvider, {
+    UnsavedChangesNotifier,
+    DocumentTitleHandler,
+} from "@refinedev/react-router-v6";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import { ConfigProvider } from "antd";
+import "@refinedev/antd/dist/reset.css";
+
+const API_URL = "https://api.fake-rest.refine.dev";
+
+const App: React.FC = () => {
+    return (
+        <BrowserRouter>
+            <GitHubBanner />
+            <ConfigProvider theme={RefineThemes.Blue}>
+                <Refine
+                    routerProvider={routerProvider}
+                    dataProvider={dataProvider(API_URL)}
+                    notificationProvider={notificationProvider}
+                    options={{
+                        warnWhenUnsavedChanges: true,
+                        syncWithLocation: true,
+                    }}
+                >
+                    <Routes>
+                        <Route path="*" element={<WelcomePage />} />
+                    </Routes>
+                    <UnsavedChangesNotifier />
+                    <DocumentTitleHandler />
+                </Refine>
+            </ConfigProvider>
+        </BrowserRouter>
+    );
+};
+
+export default App;
